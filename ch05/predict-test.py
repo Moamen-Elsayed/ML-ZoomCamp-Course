83dc87e95704f900979e261f4763ec650051617b
unknown <moamenelsaid423@yahoo.com>
2021-10-11 03:44:33 +0200

    updated

---
+++ ch05/predict-test.py
@@ -0,0 +1,38 @@
+
+import requests
+
+url = "http://192.168.1.4:9696/predict"
+
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85
+}
+
+
+response = requests.post(url, json=customer).json()
+response
+
+if response['churn'] == True:
+    print('send promo email to %s' % ('xyz-123'))
+
+
+
+
+
